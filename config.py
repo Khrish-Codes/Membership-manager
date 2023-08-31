a79a6a809ee6cf28ec04f7c9eccbba940d830223
Khrish <106378292+SpidyKhrish007@users.noreply.github.com>
2023-08-31 21:36:57 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,2 @@
+MONGODB_URI = 'mongodb+srv://Khrish:98982khrishisveryop07@cluster1.hh7bbcz.mongodb.net/?retryWrites=true&w=majority'
+TELEGRAM_BOT_TOKEN = '6622094578:AAExgt9BOiWki0aix5xW04y2xSVBvZtjPs8'
